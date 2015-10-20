9556ed5c8bfd99e61246905fb5d66ed948a4e8b8
puyokw <moconapuyopop@gmail.com>
2015-10-21 03:47:13 +0900

    fixed bugs when using web viewer

--- CocosJSGame/src/app.js
+++ CocosJSGame/src/app.js
@@ -6,6 +6,7 @@ var NumPannel=8;
 var FORCED_END_TURN=10000;
 var MaxTurn=10000;
 var currTurn=0; // 現在ターンで、0 のときは初期配置
+var targetTurn=0;
 var allBoard=[];
 var Life=[];
 var PlayerPos=[];
@@ -14,41 +15,42 @@ var PlayerCommand=[];
 // input text file
 var Arr = []; // for adding strings written in text(log)
 cc.loader.loadTxt(res.sampleLog,
-	function(obj,STR){
+	function(obj,STR){	
 	var tmpArr=[];
 	var STRtmp=STR.split("\n");
 	var i=0;
 	for(i=0;i<STRtmp.length;++i){ Arr.push(STRtmp[i]);}
+	cc.log("Arr: "+Arr.length);
+	MaxTurn=parseInt(Arr.length/47)-1; // Turn が0 origin の場合 -1 すること!!
+	cc.log("MaxTurn: "+MaxTurn);
+	var j=0;
+	for(j=0;j<=MaxTurn;++j){
+		var J=47*j;
+		currTurn=parseInt(Arr[J+0][0],10);
+		cc.log("currTurn: " + currTurn);
+		Life[currTurn]=Arr[J+1].split(" ").map(function(v) { return parseInt(v); });
+		cc.log("Life: "+Life[currTurn]);
+		var currboard =[[],[]];
+		var i=0;
+		for(i=0;i<MAP_WIDTH;++i){
+			currboard[i] = Arr[J+i+2].split(" ").map(function(v) { return parseInt(v); });
+			//cc.log(currboard[i]);
+		}
+		cc.log("currboard: "+currboard.length);
+		allBoard[currTurn]=currboard;
+		PlayerPos[currTurn]=[[],[],[],[]];
+		for(i=42;i<42+playersNum;++i){
+			var tmp=Arr[J+i].split(" ");
+			PlayerPos[currTurn][i-42][0] = parseInt(tmp[0],10);
+			PlayerPos[currTurn][i-42][1] = parseInt(tmp[1],10);
+			PlayerPos[currTurn][i-42][2] = tmp[2];
+		}
+		cc.log("PlayerPos: "+PlayerPos[currTurn]);
+		PlayerCommand[currTurn]=Arr[J+i].split(" ");
+		cc.log("PlayerCommand: "+PlayerCommand[currTurn]);
+	}
 });
-cc.log("Arr: "+Arr.length);
-MaxTurn=parseInt(Arr.length/47)-1; // Turn が0 origin の場合 -1 すること!!
-cc.log("MaxTurn: "+MaxTurn);
-var j=0;
-for(j=0;j<=MaxTurn;++j){
-	var J=47*j;
-	currTurn=parseInt(Arr[J+0][0],10);
-	cc.log("currTurn: " + currTurn);
-	Life[currTurn]=Arr[J+1].split(" ").map(function(v) { return parseInt(v); });
-	cc.log("Life: "+Life[currTurn]);
-	var currboard =[[],[]];
-	var i=0;
-	for(i=0;i<MAP_WIDTH;++i){
-		currboard[i] = Arr[J+i+2].split(" ").map(function(v) { return parseInt(v); });
-		//cc.log(currboard[i]);
-	}
-	cc.log("currboard: "+currboard.length);
-	allBoard[currTurn]=currboard;
-	PlayerPos[currTurn]=[[],[],[],[]];
-	for(i=42;i<42+playersNum;++i){
-		var tmp=Arr[J+i].split(" ");
-		PlayerPos[currTurn][i-42][0] = parseInt(tmp[0],10);
-		PlayerPos[currTurn][i-42][1] = parseInt(tmp[1],10);
-		PlayerPos[currTurn][i-42][2] = tmp[2];
-	}
-	cc.log("PlayerPos: "+PlayerPos[currTurn]);
-	PlayerCommand[currTurn]=Arr[J+i].split(" ");
-	cc.log("PlayerCommand: "+PlayerCommand[currTurn]);
-}
+
 //
 var BaseLayer = cc.Layer.extend({
     sprite:null,
@@ -75,7 +77,8 @@ var FieldLayer = cc.Layer.extend({
 	ctor:function () {
 		this._super(); // initialize 
 		var size = cc.winSize; // window size
-		var board=allBoard[currTurn];
+		var board=[[],[]];
+		board=allBoard[currTurn];
 		var i,j;
 		// field の座標
 		var fieldX=new Array();
@@ -85,8 +88,8 @@ var FieldLayer = cc.Layer.extend({
 		// 2 * n + 5 (the center of the pannel)
 		for(j=0;j<NumPannel;++j){
 			for(i=0;i<NumPannel;++i){
-				if(board[5*j+2][5*i+2]<0) continue;
 				var NUM=board[5*j+2][5*i+2];
+				if(NUM<0) continue;
 				if((i+j)%2==0){
 					this.sprite = new cc.Sprite(res.block4_png);
 				}else{
@@ -202,11 +205,12 @@ var MenuLayer = cc.Layer.extend({
 				this.sprite = new cc.Sprite(res.Menu4_png);
 			}
 			this.sprite.attr({
-				x: size.width - 100,
-				y: size.height - 35 - 75*i,
+				x: size.width,
+				y: size.height-75*i, // 画像サイズ(200*75)
 				scale: 1,
 				rotation: 0
 			});
+			this.sprite.setAnchorPoint(1, 1);	
 			this.addChild(this.sprite, 0);
 			var Labelsize=25;
 			var NAME = "Palyer" + (i+1);
@@ -219,7 +223,8 @@ var MenuLayer = cc.Layer.extend({
 			// life
 			var k;
 			for(k=0;k<INITIAL_LIFE;++k){
-				if(k < Life[currTurn][i]){
+				var tmp = Life[currTurn][i];
+				if(k<tmp){
 					this.sprite = new cc.Sprite(res.life1_png);
 				}else{
 					this.sprite = new cc.Sprite(res.life2_png);
@@ -237,16 +242,18 @@ var MenuLayer = cc.Layer.extend({
 		// Menubar(it shows parameters, such as current turn)
 		this.sprite = new cc.Sprite(res.Menu5_png);
 		this.sprite.attr({
-			x: size.width - 100,
-			y: size.height - 35 - 75*4,
+			x: size.width,
+			y: size.height - 75*4,
 			scale: 1,
 			rotation: 0
 		});
+		this.sprite.setAnchorPoint(1, 1);
 		this.addChild(this.sprite, 0);
 		var TurnLabel = new cc.LabelTTF("Turn: "+currTurn, "Arial", Labelsize);
 		TurnLabel.x = size.width - 100;
 		TurnLabel.y = size.height -35 + Labelsize - 75*4;
 		TurnLabel.setColor(cc.color(0,0,0,0));
+		//TurnLabel.setAnchorPoint(0.5,0.5);
 		this.addChild(TurnLabel, 0);
 
 		// Turn (go and back)
@@ -330,16 +337,12 @@ var MenuLayer = cc.Layer.extend({
 	}
 });
 
-
 var HelloWorldScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
-        var Blayer = new BaseLayer();
-        this.addChild(Blayer);
-        var MLayer = new MenuLayer();
-        this.addChild(MLayer); 
-        var FLayer = new FieldLayer();
-        this.addChild(FLayer);
+        this.addChild(new BaseLayer);
+        this.addChild(new MenuLayer); 
+        this.addChild(new FieldLayer);
         cc.log("height " + cc.winSize.height);
         cc.log("width " + cc.winSize.width);
     }
